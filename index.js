6257b3eae38fcc8afc74f0413c32a52f1cbbd87e
Frederick <frederick.duberville@gmail.com>
2017-04-14 15:54:41 -0400

    Calling init in your front-end page instead of the server - replace your-app-token by your actual app token

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var express = require('express')
 var app = express();
 
-var Smooch = require('smooch');
-
-Smooch.init({ appToken: process.env.APP_TOKEN });
-
 app.get('/', function (req, res) {
   res.sendFile('index.html',  {"root": __dirname});
 })
